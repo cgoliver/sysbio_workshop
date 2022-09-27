a27dad08588ca4007d237f21ffbdd575df3f49fe
Carlos Gonzalez Oliver <cgonzalez@bs-mbpr502.d.ethz.ch>
2022-09-27 23:20:36 +0200

    1

---
+++ exercises/1.py
@@ -0,0 +1,7 @@
+# this is an example of importing a function we defined in the tools folder
+from tools.test import try_me
+
+try_me()
+
+# try importing a function from a file that is in this current directory
+# ...
---
+++ exercises/tools/test.py
@@ -0,0 +1,2 @@
+def try_me():
+    print("Yay systems biology!")
